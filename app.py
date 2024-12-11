e46bea7c4e4161501c8fc0d12f50a1a39c184d21
Kanish027 <kanishmohariya1@gmail.com>
2024-12-11 23:14:29 +0530

    learned new concept

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import load_jobs_from_db
+from flask import Flask, render_template, jsonify
+from database import load_job_from_db, load_jobs_from_db
 
 app = Flask(__name__)
 
@@ -19,6 +19,13 @@ def contact():
 @app.route('/about')
 def about():
     return render_template('About.html')
+
+@app.route('/job/<id>')
+def job_details(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "<h1>Job Not Found</h1>", 404
+    return render_template('Job_Details.html', job_detail = job)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
