4e6431cd438dd322c2283347a2aeddd9f9203270
Kanish027 <kanishmohariya1@gmail.com>
2024-12-12 23:17:29 +0530

    learned new concept

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify
-from database import load_job_from_db, load_jobs_from_db
+from email.mime import application
+from flask import Flask, render_template, jsonify, request
+from database import add_application_to_db, load_job_from_db, load_jobs_from_db
 
 app = Flask(__name__)
 
@@ -26,6 +27,16 @@ def job_details(id):
     if not job:
         return "<h1>Job Not Found</h1>", 404
     return render_template('Job_Details.html', job_detail = job)
+
+@app.route('/job/<id>/apply', methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    job_detail = load_job_from_db(id)
+    add_application_to_db(id, data)
+    # Store this in DB
+    # Send an email
+    # display an acknowledgement
+    return render_template('Application_submitted.html', application=data, job=job_detail)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import email
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
@@ -27,3 +28,18 @@ def load_job_from_db(id):
             return None
         else:
             return rows[0]._asdict()
+        
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text('INSERT INTO applications (job_id, full_name, email, linked_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linked_url, :education, :work_experience, :resume_url)')
+        
+        conn.execute(query, 
+                     {'job_id': job_id, 
+                     'full_name': data['full_name'],
+                     'email': data['email'],
+                     'linked_url': data['linked_url'],
+                     'education': data['education'],
+                     'work_experience': data['work_experience'],
+                     'resume_url': data['resume_url']}
+                    )
+        conn.commit()
